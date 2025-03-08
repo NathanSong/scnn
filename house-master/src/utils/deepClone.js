f06f18a6c01806643c945282269a1ae0b0696e4e
NathanSong <157336272+NathanSong@users.noreply.github.com>
2025-03-08 20:45:23 +0800

    Delete house-master directory

--- house-master/src/utils/deepClone.js
+++
@@ -1,9 +0,0 @@
-/*深拷贝引用数据类型 */
-function deepClone(target) {
-    if (typeof target === "object" && target !== null) {
-        return JSON.parse(JSON.stringify(target));
-    } else {
-        return target;
-    }
-}
-export default deepClone
--- house-master/src/utils/request.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const request = axios.create({
-    // 指定请求的根路径
-    baseURL: 'http://localhost:8090'
-})
-export default request
